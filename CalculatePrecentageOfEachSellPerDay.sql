e9e89e5939fdf601fb1290e9a568b69b5392816a
Mateusz Kopij <sagasu@gmail.com>
2020-07-16 14:07:46 +0100

    calculate precentage using partition over

--- CalculatePrecentageOfEachSellPerDay.sql
+++ CalculatePrecentageOfEachSellPerDay.sql
@@ -6,3 +6,10 @@
   FROM [AdventureWorks2019].[Sales].[SalesOrderDetail] as su 
   inner join (select Sum([UnitPrice]) as summ, ModifiedDate as ModifiedDate from [AdventureWorks2019].[Sales].[SalesOrderDetail] group by ModifiedDate ) as so on su.ModifiedDate = so.ModifiedDate
   
+
+  ---same query but using partition over
+    select UnitPrice,
+      [ModifiedDate],
+	  Sum(UnitPrice) over (partition by [ModifiedDate]) as summ,
+	  UnitPrice/Sum(UnitPrice) over (partition by [ModifiedDate]) as precentage --too bad I need to repeat same query instead of being able to use an alias summ
+	  FROM [AdventureWorks2019].[Sales].[SalesOrderDetail]
